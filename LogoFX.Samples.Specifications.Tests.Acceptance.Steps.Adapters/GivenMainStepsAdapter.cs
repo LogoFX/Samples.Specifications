03dbf19fdc97fff900a00facfd0ee2dabe9ef0b4
godrose <godrose@gmail.com>
2016-01-25 14:59:13 +0200

    Removed explicit usage of builders inside the given adapter. Fixed the references.

--- LogoFX.Samples.Specifications.Tests.Acceptance.Steps.Adapters/GivenMainStepsAdapter.cs
+++ LogoFX.Samples.Specifications.Tests.Acceptance.Steps.Adapters/GivenMainStepsAdapter.cs
@@ -1,6 +1,4 @@
-﻿using Attest.Tests.Core;
-using LogoFX.Client.Tests.Contracts;
-using LogoFX.Samples.Specifications.Client.Data.Contracts.Dto;
+﻿using LogoFX.Samples.Specifications.Client.Data.Contracts.Dto;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 
@@ -9,14 +7,11 @@ namespace LogoFX.Samples.Specifications.Tests.Acceptance.Steps.Adapters
     [Binding]
     class GivenMainStepsAdapter
     {
-        private readonly IBuilderRegistrationService _builderRegistrationService = ScenarioHelper.Get<IBuilderRegistrationService>();
-
         [Given(@"warehouse contains the following items:")]
         public void GivenWarehouseContainsTheFollowingItems(Table table)
         {
             var warehouseItems = table.CreateSet<WarehouseItemDto>();
-            var warehouseProviderBuilder = GivenMainSteps.SetupWarehouseItems(warehouseItems);
-            _builderRegistrationService.RegisterBuilder(warehouseProviderBuilder);
+            GivenMainSteps.SetupWarehouseItems(warehouseItems);
         }
     }
 }
--- LogoFX.Samples.Specifications.Tests.Acceptance.Steps/GivenMainSteps.cs
+++ LogoFX.Samples.Specifications.Tests.Acceptance.Steps/GivenMainSteps.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Attest.Tests.Core;
+using LogoFX.Client.Tests.Contracts;
 using LogoFX.Samples.Specifications.Client.Data.Contracts.Dto;
 using LogoFX.Samples.Specifications.Client.Data.Fake.ProviderBuilders;
 
@@ -7,11 +8,11 @@ namespace LogoFX.Samples.Specifications.Tests.Acceptance.Steps
 {
     public static class GivenMainSteps
     {
-        public static WarehouseProviderBuilder SetupWarehouseItems(IEnumerable<WarehouseItemDto> warehouseItems)
+        public static void SetupWarehouseItems(IEnumerable<WarehouseItemDto> warehouseItems)
         {
             var warehouseProviderBuilder = ScenarioHelper.GetOrCreate(WarehouseProviderBuilder.CreateBuilder);
             warehouseProviderBuilder.WithWarehouseItems(warehouseItems);
-            return warehouseProviderBuilder;
+            ScenarioHelper.Get<IBuilderRegistrationService>().RegisterBuilder(warehouseProviderBuilder);
         }
     }
 }
