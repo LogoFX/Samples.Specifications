827a595f295d9317766ac35817da1e8d46d53aa8
godrose <godrose@gmail.com>
2016-01-25 23:17:47 +0200

    Added Real Provider with stub data.

---
+++ LogoFX.Samples.Specifications.Client.Data.Real.Providers/Module.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using LogoFX.Client.Bootstrapping.Adapters.Unity;
+using LogoFX.Samples.Specifications.Client.Data.Contracts.Providers;
+using Solid.Practices.Modularity;
+
+namespace LogoFX.Samples.Specifications.Client.Data.Real.Providers
+{
+    [Export(typeof(ICompositionModule))]
+    class Module : ICompositionModule<UnityContainerAdapter>
+    {
+        public void RegisterModule(UnityContainerAdapter iocContainer)
+        {
+            iocContainer.RegisterSingleton<IWarehouseProvider, WarehouseProvider>();
+        }
+    }
+}
---
+++ LogoFX.Samples.Specifications.Client.Data.Real.Providers/WarehouseProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LogoFX.Samples.Specifications.Client.Data.Contracts.Dto;
+using LogoFX.Samples.Specifications.Client.Data.Contracts.Providers;
+
+namespace LogoFX.Samples.Specifications.Client.Data.Real.Providers
+{
+    class WarehouseProvider : IWarehouseProvider
+    {
+        public IEnumerable<WarehouseItemDto> GetWarehouseItems()
+        {
+            //put here real data logic
+            return new WarehouseItemDto[]
+            {
+                new WarehouseItemDto
+                {
+                    Kind = "Acme",
+                    Price = 10,
+                    Quantity = 10
+                }
+            };           
+        }
+    }
+}
