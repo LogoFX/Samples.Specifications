18e1843adfe4156cc25ed7507e21aef719f49c09
dmitrybublik <dmitry.buba@gmail.com>
2016-10-17 19:18:21 +0300

    Fixed solution variant for using SimpleContainer.

---
+++ Samples.Specifications.Client.SimpleContainer.Launcher/AppBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+using Samples.Specifications.Client.Presentation.Shell.ViewModels;
+
+namespace Samples.Specifications.Client.Launcher
+{
+    public class AppBootstrapper : BootstrapperContainerBase<SimpleContainerAdapter>.WithRootObject<ShellViewModel>
+    {
+        public AppBootstrapper(SimpleContainerAdapter containerAdapter) 
+            : base(containerAdapter)
+        {
+        }              
+    }
+}
