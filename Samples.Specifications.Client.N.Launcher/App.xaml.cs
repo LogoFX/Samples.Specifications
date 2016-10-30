0762e2509f014915e138b057a30a997ec8ad31b2
dmitrybublik <dmitry.buba@gmail.com>
2016-10-30 15:09:09 +0200

    Added working navigation references.

---
+++ Samples.Specifications.Client.N.Launcher/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+using LogoFX.Client.Mvvm.Navigation;
+using Samples.Specifications.Client.Launcher.Shared;
+using Samples.Specifications.Client.Presentation.Shell.ViewModels;
+
+namespace Samples.Specifications.Client.Launcher
+{
+    partial class App
+    {
+        public App()
+        {
+            var containerAdapter = new ExtendedSimpleContainerAdapter();
+
+            var bootstrapper = new AppBootstrapper(containerAdapter);
+            bootstrapper
+                .UseResolver()
+                .UseShared()
+                .UseNavigation<ShellViewModel, ExtendedSimpleContainerAdapter>(containerAdapter)
+                .Initialize();
+        }
+    }
+}
---
+++ Samples.Specifications.Client.N.Launcher/AppBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+
+namespace Samples.Specifications.Client.Launcher
+{
+    public class AppBootstrapper : BootstrapperContainerBase<ExtendedSimpleContainerAdapter>
+    {
+        public AppBootstrapper(ExtendedSimpleContainerAdapter containerAdapter) 
+            : base(containerAdapter)
+        {
+        }       
+    }
+}
