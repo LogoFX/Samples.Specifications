e0a56487876b7f99a639fb29ead76480762a903f
godrose <godrose@gmail.com>
2017-01-27 07:12:06 +0000

    Reversed the launcher.shared removal

---
+++ Samples.Specifications.Client.Launcher.Shared.U/BootstrapperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using LogoFX.Bootstrapping;
+using LogoFX.Client.Mvvm.ViewModel.Services;
+using LogoFX.Client.Mvvm.ViewModelFactory.Unity;
+using Solid.Practices.IoC;
+
+namespace Samples.Specifications.Client.Launcher.Shared
+{    
+    public static class BootstrapperExtensions
+    {
+        public static IBootstrapperWithContainerAdapter<TContainerAdapter> UseShared<TContainerAdapter>(
+            this IBootstrapperWithContainerAdapter<TContainerAdapter> bootstrapper) where TContainerAdapter : IIocContainer
+        {
+            return bootstrapper.UseViewModelCreatorService().UseViewModelFactory();
+        }
+    }
+}
