0b414488488f603026329774195a21b61606656f
Gennady Verdel <godrose@gmail.com>
2017-06-16 20:39:00 +0300

    Added automagical registration of steps

--- Samples.Specifications.Tests.Steps/Module.cs
+++ Samples.Specifications.Tests.Steps/Module.cs
@@ -1,4 +1,6 @@
-﻿using Solid.Practices.IoC;
+﻿using System.Linq;
+using System.Reflection;
+using Solid.Practices.IoC;
 using Solid.Practices.Modularity;
 
 namespace Samples.Specifications.Tests.Steps
@@ -7,11 +9,15 @@ namespace Samples.Specifications.Tests.Steps
     {
         public void RegisterModule(IIocContainerRegistrator iocContainer)
         {
-            iocContainer.RegisterSingleton<GeneralSteps, GeneralSteps>();
-            iocContainer.RegisterSingleton<GivenLoginSteps, GivenLoginSteps>();
-            iocContainer.RegisterSingleton<LoginSteps, LoginSteps>();            
-            iocContainer.RegisterSingleton<MainSteps, MainSteps>();
-            iocContainer.RegisterSingleton<ExitSteps, ExitSteps>();
+            RegisterStepsAutomagically(iocContainer, Assembly.GetExecutingAssembly());
+        }
+
+        private static void RegisterStepsAutomagically(IIocContainerRegistrator iocContainer, Assembly assembly)
+        {
+            foreach (var type in assembly.DefinedTypes.Where(t => t.Name.EndsWith("Steps")))
+            {
+                iocContainer.RegisterSingleton(type, type);
+            }
         }
     }
 }
