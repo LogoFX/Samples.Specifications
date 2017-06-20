1768191fc3a79971ab5e22c9dfacf49864e785be
Gennady Verdel <godrose@gmail.com>
2017-06-20 15:15:04 +0300

    Added support for dynamic discovery of builders in end to end tests

--- Samples.Specifications.Tests.EndToEnd.Infra.Providers/Module.cs
+++ Samples.Specifications.Tests.EndToEnd.Infra.Providers/Module.cs
@@ -6,6 +6,7 @@ using JetBrains.Annotations;
 using LogoFX.Client.Testing.EndToEnd.FakeData.Modularity;
 using LogoFX.Client.Testing.EndToEnd.FakeData.Shared;
 using Samples.Specifications.Client.Data.Fake.ProviderBuilders;
+using Solid.Practices.Composition;
 using Solid.Practices.IoC;
 
 namespace Samples.Specifications.Tests.EndToEnd.Infra.Providers
@@ -13,21 +14,25 @@ namespace Samples.Specifications.Tests.EndToEnd.Infra.Providers
     [UsedImplicitly]
     public class Module : ProvidersModuleBase
     {
+        private const string ProvidersAssemblyEnding = "Contracts.Providers";
+        private const string BuildersAssemblyEnding = "Fake.ProviderBuilders";
+
         protected override void OnRegisterProviders(IIocContainerRegistrator iocContainer)
         {
             base.OnRegisterProviders(iocContainer);
+            //TODO: move the code below to the packages
             const string builderEnding = "Builder";
             const string providerEnding = "Provider";
-            var providersAssemblyName = "Samples.Client.Data.Contracts.Providers";
-            var providersAssembly = Assembly.Load(new AssemblyName(providersAssemblyName));
-            var matchingProvidersTypes = providersAssembly.DefinedTypes
+            var assembliesProvider = new ProvidersAssemblySourceProvider(PlatformProvider.Current.GetRootPath());
+            var allAssemblies = assembliesProvider.Assemblies.ToArray();            
+            var providersAssemblies = allAssemblies.Where(t => t.GetName().Name.EndsWith(ProvidersAssemblyEnding));
+            var matchingProvidersTypes = providersAssemblies.SelectMany(k => k.DefinedTypes
                 .Where(t => t.IsInterface && t.Name.EndsWith(providerEnding))
-                .Select(t => t.AsType()).ToArray();
-            var buildersAssemblyName = "Samples.Specifications.Client.Data.Fake.ProviderBuilders";            
-            var buildersAssembly = Assembly.Load(new AssemblyName(buildersAssemblyName));            
-            var matchingBuildersTypes = buildersAssembly.DefinedTypes
-                .Where(t => t.IsInterface == false && t.Name.EndsWith(builderEnding))
-                .Select(t => t.AsType());
+                .Select(t => t.AsType())).ToArray();
+            var buildersAssemblies = allAssemblies.Where(t => t.GetName().Name.EndsWith(BuildersAssemblyEnding));
+            var matchingBuildersTypes = buildersAssemblies.SelectMany(k => k.DefinedTypes
+                .Where(t => t.IsInterface == false && t.IsAbstract == false && t.Name.EndsWith(builderEnding))
+                .Select(t => t.AsType())).ToArray();
             var typeMatches = new Dictionary<Type, Type>();
             foreach (var type in matchingBuildersTypes)
             {
@@ -63,5 +68,19 @@ namespace Samples.Specifications.Tests.EndToEnd.Infra.Providers
                 }
             }
         }
+
+        public class ProvidersAssemblySourceProvider : AssemblySourceProviderBase
+        {
+            public ProvidersAssemblySourceProvider(string rootPath) : base(rootPath)
+            {
+            }
+
+            protected override string[] ResolveNamespaces()
+            {
+                return new[] {ProvidersAssemblyEnding, BuildersAssemblyEnding};
+            }
+        }
     }
+
+    
 }
