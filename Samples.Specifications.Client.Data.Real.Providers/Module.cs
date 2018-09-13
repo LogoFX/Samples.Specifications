adbb00ab0c1ddadba82aa4175712e711ee6d7c7c
Gennady Verdel <godrose@gmail.com>
2018-09-14 00:42:26 +0300

    Removed obsolete code

--- Samples.Specifications.Client.Data.Real.Providers/Module.cs
+++ Samples.Specifications.Client.Data.Real.Providers/Module.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using RestSharp;
 using Samples.Client.Data.Contracts.Providers;
 using Samples.Specifications.Client.Data.Real.Providers.Properties;
@@ -13,28 +12,10 @@ namespace Samples.Specifications.Client.Data.Real.Providers
     {
         public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
         {
-            dependencyRegistrator.RegisterSingleton<ILoginProvider, LoginProvider>();
-            dependencyRegistrator.RegisterSingleton<IWarehouseProvider, WarehouseProvider>();
-            dependencyRegistrator.RegisterSingleton<IEventsProvider, EventsProvider>();
-            dependencyRegistrator.RegisterSingleton(() => new RestClient(RetrieveEndpoint()));
-        }
-
-        private string RetrieveEndpoint()
-        {
-            return Settings.Default.ServerEndpoint;
-            //TODO: This doesn't work in case of Integration Tests - The runner doesn't copy the .config file to the temporary folder
-            //var exeConfigPath = GetType().Assembly.Location;
-            //var config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
-            //return GetAppSetting(config, "ServerEndpoint");
-        }
-
-        private string GetAppSetting(Configuration config, string key)
-        {
-            var element = config.AppSettings.Settings[key];
-            var value = element?.Value;
-            return string.IsNullOrEmpty(value) ? string.Empty : value;
-        }
+            dependencyRegistrator.AddSingleton<ILoginProvider, LoginProvider>()
+                .AddSingleton<IWarehouseProvider, WarehouseProvider>()
+                .AddSingleton<IEventsProvider, EventsProvider>()
+                .AddSingleton(() => new RestClient(Settings.Default.ServerEndpoint));
+        }             
     }
-
-
 }
