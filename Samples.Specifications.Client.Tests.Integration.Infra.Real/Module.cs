87720072350f16aa281649654728634a3b37a0c8
Gennady Verdel <godrose@gmail.com>
2018-09-13 15:14:23 +0300

    Added Infra.Real and InegrationWithReal configuration - need to modify steps to removed dependency on Fake.ProviderBuilders in this assembly

---
+++ Samples.Specifications.Client.Tests.Integration.Infra.Real/Module.cs
@@ -0,0 +1,17 @@
+﻿using Attest.Testing.Contracts;
+using Attest.Testing.Core.FakeData;
+using LogoFX.Client.Testing.Contracts;
+using Samples.Specifications.Client.Tests.Integration.Infra.Real;
+using Solid.Practices.IoC;
+using Solid.Practices.Modularity;
+
+namespace Samples.Specifications.Client.Tests.Integration.Infra.Fake
+{
+    class Module : ICompositionModule<IDependencyRegistrator>
+    {
+        public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
+        {
+            dependencyRegistrator.AddSingleton<IStartApplicationService, StartApplicationService>();
+        }
+    }
+}
---
+++ Samples.Specifications.Client.Tests.Integration.Infra.Real/StartApplicationService.cs
@@ -0,0 +1,47 @@
+﻿using LogoFX.Client.Testing.Integration;
+using LogoFX.Client.Testing.Integration.SpecFlow;
+using Samples.Specifications.Client.Presentation.Shell.ViewModels;
+using Samples.Specifications.Client.Tests.Integration.Infra.Core;
+using Samples.Specifications.Client.Tests.Integration.Infra.Shared;
+
+namespace Samples.Specifications.Client.Tests.Integration.Infra.Real
+{
+    class StartApplicationService : StartApplicationServiceBase
+    {
+        private readonly StructureHelper _structureHelper;
+
+        public StartApplicationService(StructureHelper structureHelper)
+        {
+            _structureHelper = structureHelper;
+        }
+
+        protected override void RegisterFakes()
+        {
+            base.RegisterFakes();            
+            //TODO: Strictly speaking this is not an appropriate place
+            //for root object initialization - need to rethink the whole initialization process
+            //for integration tests which MUST initialize their root object after the arrange step
+            var bootstrapperBridge = new BootstrapperBridge();
+            bootstrapperBridge.InitializeRootObject();
+            //TODO: the same-thread functionality must be set after the bootstrapper init is finished
+            SetupCore();
+        }
+
+        private static void SetupCore()
+        {
+            LogoFX.Client.Testing.Shared.TestHelper.Setup();
+        }
+
+        protected override void OnStart(object rootObject)
+        {
+            base.OnStart(rootObject);
+            RootObjectHelper.InitializeRootObject(_structureHelper, rootObject);
+        }
+
+        class BootstrapperBridge : IntegrationTestsBase<ShellViewModel, TestBootstrapper.SpecBased>.
+            WithExplicitRootObjectCreation
+        {
+
+        }
+    }
+}
