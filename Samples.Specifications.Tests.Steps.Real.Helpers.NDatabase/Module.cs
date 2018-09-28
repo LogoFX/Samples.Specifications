28449041557eb01199d5de179772a2e325d6eaf6
Gennady Verdel <godrose@gmail.com>
2018-09-28 12:29:28 +0300

    Using steps helpers for mongodb only

---
+++ Samples.Specifications.Tests.Steps.Real.Helpers.NDatabase/Module.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Specifications.Tests.Steps.Real.Helpers.MongoDb
+{
+    class Module : ICompositionModule<IDependencyRegistrator>
+    {
+    }
+}
--- Samples.Specifications.Tests.Steps.Real.Helpers.NDatabase/Properties/AssemblyInfo.cs
+++ Samples.Specifications.Tests.Steps.Real.Helpers.NDatabase/Properties/AssemblyInfo.cs
@@ -1,34 +1,35 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Samples.Specifications.Tests.Steps.Real.Helpers")]
+[assembly: AssemblyTitle("Samples.Specifications.Tests.Steps.Real.Helpers.NDatabase")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Samples.Specifications.Tests.Steps.Real.Helpers")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Samples.Specifications.Tests.Steps.Real.Helpers.NDatabase")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("695f7796-e668-49b8-93a7-58f4b8a2c3aa")]
+[assembly: Guid("5939fc8f-f7a5-4bd0-a79f-f37330951295")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
