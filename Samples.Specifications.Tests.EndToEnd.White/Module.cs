8e2ee00835083ac2ab7629235eeaa56dd714b94e
Gennady Verdel <godrose@gmail.com>
2018-10-01 10:56:03 +0300

    Using explicit E2E.White project

---
+++ Samples.Specifications.Tests.EndToEnd.White/Module.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Attest.Testing.Contracts;
+using JetBrains.Annotations;
+using LogoFX.Client.Testing.Contracts;
+using LogoFX.Client.Testing.EndToEnd.White;
+using Solid.Practices.IoC;
+using Solid.Practices.Modularity;
+
+namespace Samples.Specifications.Tests.EndToEnd
+{
+    [UsedImplicitly]
+    internal sealed class Module : ICompositionModule<IDependencyRegistrator>
+    {
+        public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
+        {            
+            dependencyRegistrator                
+                .RegisterAutomagically(
+                Assembly.LoadFrom("Samples.Specifications.Tests.Contracts.dll"),
+                Assembly.GetExecutingAssembly())               
+                .AddSingleton<StructureHelper, StructureHelper>()
+                .AddSingleton<IApplicationFacade, ApplicationFacade>()                
+                .AddSingleton<ITeardownService, TeardownService>();
+        }
+    }
+}
--- Samples.Specifications.Tests.EndToEnd/Module.cs
+++ Samples.Specifications.Tests.EndToEnd/Module.cs
@@ -1,28 +1,17 @@
-﻿using System.Reflection;
-using Attest.Testing.Contracts;
-using JetBrains.Annotations;
-using LogoFX.Client.Testing.Contracts;
+﻿using LogoFX.Client.Testing.Contracts;
 using LogoFX.Client.Testing.EndToEnd;
-using LogoFX.Client.Testing.EndToEnd.White;
 using Solid.Practices.IoC;
 using Solid.Practices.Modularity;
 
 namespace Samples.Specifications.Tests.EndToEnd
 {
-    [UsedImplicitly]
     internal sealed class Module : ICompositionModule<IDependencyRegistrator>
     {
         public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
-        {            
-            dependencyRegistrator                
-                .RegisterAutomagically(
-                Assembly.LoadFrom("Samples.Specifications.Tests.Contracts.dll"),
-                Assembly.GetExecutingAssembly())
+        {
+            dependencyRegistrator
                 .AddSingleton<IApplicationPathInfo, ApplicationPathInfo>()
-                .AddSingleton<StructureHelper, StructureHelper>()
-                .AddSingleton<IApplicationFacade, ApplicationFacade>()
-                .AddSingleton<IStartClientApplicationService, StartClientApplicationService>()
-                .AddSingleton<ITeardownService, TeardownService>();
+                .AddSingleton<IStartClientApplicationService, StartClientApplicationService>();
         }
     }
 }
