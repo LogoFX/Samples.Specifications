b15058d6f116cfe9fc76f9d63004a67107a436ab
Gennady Verdel <godrose@gmail.com>
2018-10-09 08:49:55 +0300

    Updated dependent packages

--- Samples.Specifications.Client.Data.Fake.Providers.ContextBased/Module.cs
+++ Samples.Specifications.Client.Data.Fake.Providers.ContextBased/Module.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Linq;
-using Attest.Fake.Registration;
-using Attest.Testing.Core.FakeData.Modularity;
+﻿using Attest.Testing.Core.FakeData.Modularity;
 using Attest.Testing.Core.FakeData.Shared;
 using JetBrains.Annotations;
 using Samples.Specifications.Client.Data.Fake.Shared;
-using Solid.Patterns.Builder;
 using Solid.Practices.IoC;
 
 namespace Samples.Specifications.Client.Data.Fake.Providers.ContextBased
@@ -15,35 +11,9 @@ namespace Samples.Specifications.Client.Data.Fake.Providers.ContextBased
     {       
         protected override void OnRegisterProviders(IDependencyRegistrator dependencyRegistrator)
         {
-            base.OnRegisterProviders(dependencyRegistrator);                        
-            foreach (var match in ConventionsHelper.FindContractToBuilderMatches())
-            {
-                var contractType = match.Key;
-                var builderType = match.Value;                
-                var builderInstance = BuilderFactory.CreateBuilderInstance(builderType);
-                RegisterAllBuildersInternal(dependencyRegistrator, (IBuilder) builderInstance, builderType,
-                    contractType);
-            }            
+            base.OnRegisterProviders(dependencyRegistrator);
+            dependencyRegistrator.RegisterBuildersProducts(ConventionsHelper.FindContractToBuilderMatches(),
+                BuilderFactory.CreateBuilderInstance);                  
         }
-
-        private static void RegisterAllBuildersInternal(
-            IDependencyRegistrator dependencyRegistrator,
-            IBuilder builderInstance, 
-            Type builderType,
-            Type contractType)
-        {
-            var builders = BuildersCollectionContext.GetBuilders(builderType).OfType<IBuilder>().ToArray();
-            if (builders.Length == 0)
-            {
-                RegistrationHelper.RegisterBuilder(dependencyRegistrator, contractType, builderInstance);                
-            }
-            else
-            {
-                foreach (var builder in builders)
-                {
-                    RegistrationHelper.RegisterBuilder(dependencyRegistrator, contractType, builder);
-                }
-            }
-        }        
     }    
 }
