3f69bb475c902048642edd4e2a90d2b3d5e57670
Gennady Verdel <godrose@gmail.com>
2018-10-14 09:57:57 +0300

    Using SetDir approach for Server facade

--- Samples.Specifications.Server.Facade/Bootstrapper.cs
+++ Samples.Specifications.Server.Facade/Bootstrapper.cs
@@ -1,37 +1,14 @@
-﻿using System.IO;
-using LogoFX.Server.Bootstrapping;
+﻿using LogoFX.Server.Bootstrapping;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Samples.Specifications.Server.Facade
 {
-    public class Bootstrapper : BootstrapperBase
+    internal sealed class Bootstrapper : BootstrapperBase
     {
         public Bootstrapper(IServiceCollection dependencyRegistrator) : base(dependencyRegistrator)
         {
         }
 
-        public override string ModulesPath => CalculateModulesPath();
-
-        private static string CalculateModulesPath()
-        {
-            var configuration = GetCurrentConfiguration();
-            return configuration == string.Empty
-                ? "."
-                : Path.Combine(new[] {"..", "bin", "Server", configuration});
-        }
-
-        private static string GetCurrentConfiguration() =>
-#if RELEASE
-        string.Empty
-#elif DEBUGWITHFAKE
-        "DebugWithFake"
-#elif DEBUGWITHREAL
-        "DebugWithReal"
-#else
-            "Debug"
-#endif
-        ;
-
-        public override string[] Prefixes => new[] {"Samples.Specifications"};
+        public override string[] Prefixes => new[] { "Samples.Specifications" };
     }
 }
--- Samples.Specifications.Server.Facade/Program.cs
+++ Samples.Specifications.Server.Facade/Program.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Samples.Specifications.Server.Facade
 {
     public class Program
     {
-        public static void Main(string[] args) => BuildWebHost(args).Run();
+        public static void Main(string[] args)
+        {
+            var executingDir = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            Directory.SetCurrentDirectory(executingDir);
 
-        public static IWebHost BuildWebHost(string[] args) =>
+            //TODO: Have to provide the start urls during web host build                 
+            BuildWebHost(args, new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build()).Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args, IConfiguration configuration) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseConfiguration(configuration)
                 .Build();
     }
 }
