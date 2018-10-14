5ecf840a95ecd97e084fba9faa79f65db9aeae95
Gennady Verdel <godrose@gmail.com>
2018-10-14 06:13:45 +0300

    Using local version of modules registration middleware

--- Samples.Specifications.Tests.Infra.Launcher/Startup.cs
+++ Samples.Specifications.Tests.Infra.Launcher/Startup.cs
@@ -1,5 +1,8 @@
-﻿using LogoFX.Bootstrapping;
+﻿using Solid.Bootstrapping;
+using Solid.Practices.Composition.Contracts;
 using Solid.Practices.IoC;
+using Solid.Practices.Middleware;
+using Solid.Practices.Modularity;
 
 namespace Samples.Specifications.Tests.Infra.Launcher
 {
@@ -10,6 +13,20 @@ namespace Samples.Specifications.Tests.Infra.Launcher
         public Startup(IDependencyRegistrator dependencyRegistrator) => _dependencyRegistrator = dependencyRegistrator;
 
         public void Initialize() => new Bootstrapper(_dependencyRegistrator)
-            .Use(new RegisterCompositionModulesMiddleware<Bootstrapper>()).Initialize();
+            .Use(new ModulesRegistrationMiddleware<Bootstrapper>()).Initialize();
+
+        private sealed class ModulesRegistrationMiddleware<TBootstrapper> : IMiddleware<TBootstrapper>
+            where TBootstrapper : class, ICompositionModulesProvider, IHaveRegistrator
+        {
+            public TBootstrapper Apply(TBootstrapper @object)
+            {
+                var middlewares = new[]
+                {
+                    new ContainerRegistrationMiddleware<IDependencyRegistrator, IDependencyRegistrator>(@object.Modules)
+                };
+                MiddlewareApplier.ApplyMiddlewares(@object.Registrator, middlewares);
+                return @object;
+            }
+        }
     }
 }
