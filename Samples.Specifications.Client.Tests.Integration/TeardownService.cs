951e139b548e909cc2c9745d5f9f1bf208d8ad19
Gennady Verdel <godrose@gmail.com>
2019-01-01 22:16:47 +0200

    Extracted cross-plat Integration.Core

--- Samples.Specifications.Client.Tests.Integration/TeardownService.cs
+++ Samples.Specifications.Client.Tests.Integration/TeardownService.cs
@@ -1,14 +1,17 @@
 ﻿using Attest.Testing.Contracts;
+using JetBrains.Annotations;
+using LogoFX.Client.Testing.Shared.Caliburn.Micro;
 using Samples.Client.Model.Shared;
 
 namespace Samples.Specifications.Client.Tests.Integration
 {
+    [UsedImplicitly]
     internal sealed class TeardownService : ITeardownService
     {
         public void Teardown()
         {
             UserContext.Current = null;
-            LogoFX.Client.Testing.Shared.Caliburn.Micro.TestHelper.Teardown();
+            TestHelper.Teardown();
         }
     }
 }
