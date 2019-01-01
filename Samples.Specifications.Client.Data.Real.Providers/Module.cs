e0fce04fac3aebd0852b7757053b79714c4c80f2
Gennady Verdel <godrose@gmail.com>
2019-01-01 18:50:49 +0200

    Migrated Real.Providers to .NETStandard

--- Samples.Specifications.Client.Data.Real.Providers/Module.cs
+++ Samples.Specifications.Client.Data.Real.Providers/Module.cs
@@ -2,7 +2,6 @@
 using JetBrains.Annotations;
 using RestSharp;
 using Samples.Client.Data.Contracts.Providers;
-using Samples.Specifications.Client.Data.Real.Providers.Properties;
 using Solid.Practices.IoC;
 using Solid.Practices.Modularity;
 
@@ -11,11 +10,14 @@ namespace Samples.Specifications.Client.Data.Real.Providers
     [UsedImplicitly]
     internal sealed class Module : ICompositionModule<IDependencyRegistrator>
     {
+        //TODO: To be put into config
+        private const string ServerEndPoint = "http://localhost:32064";
+
         public void RegisterModule(IDependencyRegistrator dependencyRegistrator) => dependencyRegistrator
             .RegisterAutomagically(
                 Assembly.LoadFrom(AssemblyInfo.AssemblyName),
                 Assembly.GetExecutingAssembly())
             .AddSingleton<IRequestFactory, RestRequestFactory>()
-            .AddSingleton(() => new RestClient(Settings.Default.ServerEndpoint));
+            .AddSingleton(() => new RestClient(ServerEndPoint));
     }
 }
