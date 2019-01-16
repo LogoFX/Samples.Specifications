fc775345a4dad7a1df110bfb5f02b26685e6802f
Gennady Verdel <godrose@gmail.com>
2019-01-16 18:53:46 +0200

    Removing unneeded registration of ShellViewModel

--- Samples.Specifications.Client.Tests.Integration.Infra.Shared/TestBootstrapper.cs
+++ Samples.Specifications.Client.Tests.Integration.Infra.Shared/TestBootstrapper.cs
@@ -1,13 +1,13 @@
 ﻿using LogoFX.Client.Bootstrapping;
 using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
 using Samples.Specifications.Client.Launcher.Shared;
-using Samples.Specifications.Client.Presentation.Shell.ViewModels;
+using Samples.Specifications.Client.Presentation.Shell.Contracts.ViewModels;
 using Solid.Practices.Composition;
 
 namespace Samples.Specifications.Client.Tests.Integration.Infra.Shared
 {
     public class TestBootstrapper : TestBootstrapperContainerBase<ExtendedSimpleContainerAdapter>
-        .WithRootObject<ShellViewModel>
+        .WithRootObject<IShellViewModel>
     {
         public TestBootstrapper() :
             base(new ExtendedSimpleContainerAdapter(), new BootstrapperCreationOptions
